0703e9d98f9aa59c1a823ecebf444cac0747b910
Noah Clark <noahammon00@gmail.com>
2023-04-26 11:07:21 -0600

    added addpost and get posts functions

--- server/controllers/post.ts
+++ server/controllers/post.ts
@@ -1,16 +1,54 @@
 import { Request, Response } from "express";
+import User from "../models/user";
+import Post from "../models/post";
 
 module.exports = {
-    getAllPosts: (req: Request, res: Response) => {
-        console.log('all posts')
+    getAllPosts: async (req: Request, res: Response) => {
+        try {
+            const posts = await Post.findAll({
+                where: {privateStatus: false},
+                include: [{
+                    model: User,
+                    required: true,
+                    attributes: [`username`]
+                }]
+            })
+            res.status(200).send(posts)
+        } catch (error) {
+            console.log('ERROR IN getAllPosts')
+            console.log(error)
+            res.sendStatus(400)
+        }
     },
 
-    getCurrentUserPosts: (req: Request, res: Response) => {
-        console.log('user posts')
+    getCurrentUserPosts: async (req: Request, res: Response) => {
+        const { userId } = req.params
+        try {
+            const posts = await Post.findAll({
+                where: {userId: userId},
+                include: [{
+                    model: User,
+                    required: true,
+                    attributes: [`username`]
+                }]
+            })
+            res.status(200).send(posts)
+        } catch (error) {
+            console.log('ERROR IN getCurrentUserPosts')
+            console.log(error)
+            res.sendStatus(400)
+        }
     },
 
-    addPost: (req: Request, res: Response) => {
-        console.log('add posts')
+    addPost: async (req: Request, res: Response) => {
+        try {
+            const {title, content, status, userId} = req.body
+            await Post.create({title: title, content: content, privateStatus: status, userId: userId})
+            res.sendStatus(200)
+        } catch (err) {
+            console.error(err)
+            res.sendStatus(400)
+        }
     },
 
     editPost: (req: Request, res: Response) => {
